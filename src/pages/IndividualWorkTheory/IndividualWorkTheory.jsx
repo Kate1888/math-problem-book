6795911657bb8d78fcf2ffa4026eea182182f371
LAPTOP-B49IE37S\Kate <kate16ko@icloud.com>
2024-09-01 15:07:11 +0300

    Страница теории по самостоятельной

---
+++ src/pages/IndividualWorkTheory/IndividualWorkTheory.jsx
@@ -0,0 +1,44 @@
+import "./IndividualWorkTheory.css";
+
+function IndividualWorkTheory() {
+  return (
+    <div className="IndividualWorkTheory">
+      <h1>
+        <p align="center">
+          Самостоятельная работа по теме нахождение наименьшего общего кратного
+        </p>
+      </h1>
+      <a href="../index.html">
+        <h3>
+          <p style="margin-left:15%"> &#8592; Оглавление</p>
+        </h3>
+      </a>
+      <div class="links">
+        <p>
+          <a href="page6.html">
+            <h2 style="margin-left:15%">
+              <U>Теория</U>
+            </h2>
+          </a>
+        </p>
+        <p>
+          <a href="page6.1.html">
+            <h3> Тренажер</h3>
+          </a>
+        </p>
+      </div>
+
+      <table
+        border="1"
+        style="margin-left:25%; margin-top:0%"
+        bgcolor="#FFFFFF"
+      >
+        <th style="width: 891; height: 1287;" align="left">
+          <img src="NOK.png" style="width: 891; height: 1287;" />
+        </th>
+      </table>
+    </div>
+  );
+}
+
+export default IndividualWorkTheory;
