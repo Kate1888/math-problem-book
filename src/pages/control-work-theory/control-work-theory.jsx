b0259a15ad623d6da145a41daf0c6b3a337ef179
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2025-01-19 19:40:16 +0300

    Текст для теории по контрольным работам

--- src/pages/control-work-theory/control-work-theory.jsx
+++ src/pages/control-work-theory/control-work-theory.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams, Link as RouterLink } from "react-router-dom";
-import { Box, Heading, VStack, List, ListItem, Link } from "@chakra-ui/react";
+import { Box, Heading, VStack, List, Link, Text } from "@chakra-ui/react";
 import Layout from "../shared-components/layout/layout"; // Подключаем Layout
 import getControlWork from "../../data/control-works/works"; // Получение данных контрольной работы
 import getIndividualWorks from "../../data/individual-works/works"; // Получение данных самостоятельных работ
@@ -30,15 +30,29 @@ const ControlWorkTheory = () => {
     return (
         <Layout testButton={testButton}>
             <VStack align="start" spacing={6} p={6}>
-                {/* Заголовок контрольной работы */}
-                <Heading size="lg" textAlign="center" w="full">
-                    {workData.theme}
-                </Heading>
+                <VStack align="start" spacing={6} p={6}>
+                    {/* Заголовок теории */}
+                    <Heading size="lg" textAlign="center" w="full">
+                        Теоретическая подготовка для контрольной работы
+                    </Heading>
+
+                    {/* Описание */}
+                    <Text fontSize="lg" mb={6}>
+                        Контрольная работа охватывает несколько важных тем, которые требуют хорошего понимания теоретического материала.
+                        Для подготовки к контрольной работе вам рекомендуется пройти самостоятельные работы, связанные с каждой темой, чтобы лучше подготовиться.
+                        Ниже представлен список тем, с которыми вам предстоит ознакомиться. Каждая тема включает ссылки на самостоятельные работы, которые помогут вам углубиться в теорию.
+                        Нажмите на тему, чтобы перейти к соответствующему материалу.
+                    </Text>
+                    {/* Дополнительная информация о начале тренажера */}
+                    <Text fontSize="lg" mb={6}>
+                        Чтобы начать прохождение контрольной работы, нажмите на кнопку <strong>"Тренажер"</strong> сверху.
+                    </Text>
+                </VStack>
 
                 {/* Список тем для самостоятельных работ */}
-                <Box w="full">
+                <Box w="full" align="start" spacing={4}>
                     <Heading size="md" mb={4}>
-                        Список тем для самостоятельных работ:
+                        Список самостоятельных работ для подготовки к контрольной работе по теме "{workData.theme}":
                     </Heading>
                     <List.Root spacing={4}>
                         {individualWorks.map((individualWork) => (
