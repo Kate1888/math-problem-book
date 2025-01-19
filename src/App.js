ff1e2ce6d60caeb3369370d4506914d37d52c7fc
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2025-01-19 21:37:55 +0300

    Правка роутинга для пайплайнов

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/home/home';
 import IndividualWorkTheory from './pages/individual-work-theory/individual-work-theory';
 import IndividualWorkTest from "./pages/individual-work-test/individual-work-test";
